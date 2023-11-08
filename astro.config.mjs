27734bf5cafd24a781d26317f1dd4ffe94ac58e0
chenliwen <liwen.chen@mobvista.com>
2023-11-08 18:05:25 +0800

    :tada: first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,9 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
