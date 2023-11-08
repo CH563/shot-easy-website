74695a5330c2ae0d2812f08e4e29b85506144046
chenliwen <liwen.chen@mobvista.com>
2023-11-08 17:45:53 +0800

    :tada: first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
+        extend: {},
+    },
+    plugins: [],
+};
