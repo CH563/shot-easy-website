a75f087765d42595adfe43cbefe1559bdb6dc9a8
chenliwen <liwen.chen@mobvista.com>
2024-06-05 14:40:15 +0800

    i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
-import { CONFIG } from "./src/lib/config";
+import { CONFIG, LANGUAGES_CODE } from "./src/lib/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +16,12 @@ export default defineConfig({
         prefixDefaultLocale: false
     }
   },
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react(), sitemap({
+    i18n: {
+      defaultLocale: "en",
+      locales: LANGUAGES_CODE
+    }
+  })],
   output: "server",
   adapter: vercel({
     webAnalytics: {
--- src/lib/config.js
+++ src/lib/config.js
@@ -5,6 +5,13 @@ export const LANGUAGES = {
     ja: '日本語',
     'zh-CN': '简体中文'
 };
+export const LANGUAGES_CODE = {
+    en: 'en-US',
+    es: 'es-ES',
+    fr: 'fr-FR',
+    ja: 'ja-JP',
+    'zh-CN': 'zh-CN'
+};
 
 const locals = Object.keys(LANGUAGES);
 
