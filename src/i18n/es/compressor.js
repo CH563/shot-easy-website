6d32353a03f8c2b8c097c508cb904516f161ca94
chenliwen <liwen.chen@mobvista.com>
2024-06-22 22:19:01 +0800

    add extension

--- src/i18n/es/compressor.js
+++ src/i18n/es/compressor.js
@@ -7,5 +7,19 @@ export default {
     onlineCont2: 'es una compresión puramente local sin ninguna lógica del lado del servidor, es completamente segura.',
     why: '¿Por qué querrías comprimir imágenes?',
     whyCont1: 'Dependiendo de la fuente de una imagen, el archivo podría ser bastante grande. Una JPG de una cámara DSLR profesional, por ejemplo, podría tener docenas de megabytes. Dependiendo de tus necesidades, esto podría ser demasiado grande. Comprimir esta imagen sería muy útil.',
-    whyCont2: 'Asimismo, podrías tener imágenes grandes en tu teléfono. Estas imágenes podrían estar ocupando mucho espacio en el disco duro y evitando que tomes más fotos. Comprimirlas podría liberar más almacenamiento interno, solucionando este problema.'
+    whyCont2: 'Asimismo, podrías tener imágenes grandes en tu teléfono. Estas imágenes podrían estar ocupando mucho espacio en el disco duro y evitando que tomes más fotos. Comprimirlas podría liberar más almacenamiento interno, solucionando este problema.',
+    vsTitle: 'ShotEasy Compress vs TinyPNG Compress',
+    vsTitle1: 'ShotEasy Compress',
+    vsTitle2: 'TinyPNG Compress',
+    vsCont1: 'Uso de bibliotecas nativas C++: libimagequant / libpng / zlib, compiladas en WebAssembly, utilizadas en la web.',
+    vsCont2: 'Compresión a nivel de memoria mediante codificación nativa.',
+    vsCont3: 'Después de cargar los recursos, uso local puro en el navegador, sin necesidad de subir archivos',
+    vsCont4: 'Gratis, sin límite de tamaño, sin límite de número, soporta carpetas multinivel',
+    vsCont5: 'Puramente local y de código abierto, garantizando privacidad y seguridad.',
+    vsCont6: 'El sitio web TinyPNG logra la compresión de imágenes mediante el uso de algoritmos de compresión inteligentes. Estos algoritmos, basados en tecnologías de aprendizaje profundo y aprendizaje automático, pueden detectar y eliminar detalles innecesarios e información redundante en las imágenes, reduciendo así el tamaño de los archivos de imagen.',
+    vsCont7: 'El algoritmo de compresión de TinyPNG principalmente detecta píxeles no importantes en las imágenes y los reemplaza con representaciones más simples, reduciendo así el tamaño del archivo.',
+    vsCont8: 'TinyPNG permite subir múltiples imágenes a la vez, procesando hasta 20 imágenes simultáneamente.',
+    vsCont9: 'Solo soporta formatos de imagen como PNG, JPEG, WebP, no soporta GIF, SVG y otros formatos',
+    vsCont10: 'Soporte máximo de 5MB por imagen, puede tener limitaciones para imágenes de ultra alta resolución',
+    vsCont11: 'Riesgo de privacidad, los usuarios necesitan subir las imágenes a los servidores de TinyPNG para la compresión.'
 }
